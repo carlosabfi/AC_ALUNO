8d7e41673275351a8de70aa597aa00cb4efd2403
Carlos Alberto Bandeira Filho <80410556+carlosabfi@users.noreply.github.com>
2022-09-12 10:24:53 -0300

    Add files via upload

---
+++ formulario.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
+app.config['MYSQL_DATABASE_DB'] = 'alunos'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+mysql.init_app(app)
+
+
+@app.route('/')
+def main():
+    return render_template('formulario.html')
+
+
+@app.route('/aluno', methods=['POST', 'GET'])
+def aluno():
+    nome = request.form['nome']
+    cpf = request.form['cpf']
+    endereco = request.form['endereco']
+    if nome and cpf and endereco:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute('insert into tbl_aluno (user_name, user_cpf, user_endereco) VALUES (%s, %s, %s)', (nome, cpf, endereco))
+        conn.commit()
+    return render_template('formulario.html')
+
+
+@app.route('/listaraluno', methods=['POST', 'GET'])
+def listaraluno():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('select user_name, user_cpf, user_endereco alunos_AC_ALUNO')
+    data = cursor.fetchall()
+    conn.commit()
+    return render_template('lista.html', datas=data)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8888))
+    app.run(host='0.0.0.0', port=port)
